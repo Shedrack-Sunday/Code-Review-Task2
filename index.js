b748edc42820c06a8b1b38620a78cea2d2f340ff
Shedrack Sunday <Hannania002@gmail.com>
2023-01-05 15:50:21 +0100

    Add fix to mobile menu close button

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const mobilenav = document.getElementById('mobile-nav');
 button.onclick = function () {
   mobilenav.style.display = 'block';
 };
+
+mobileNavCloseButton.onclick = function () {
+  mobilenav.style.display = "none";
+};
